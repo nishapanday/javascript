da4a7d3bde577d9ba569bcae6709022c8a168452
nisha.xdezo@gmail.com <nisha.xdezo@gmail.com>
2021-10-12 09:04:01 -0700

    Variable

---
+++ js/index.js
@@ -0,0 +1,62 @@
+// printing something
+console.log("Hello javascript");
+
+//decleration varibles
+var x = 5;
+
+let name = "nisha";
+
+name = "panday";
+
+console.log(name);
+
+const pi = 3.124578;
+
+console.log(pi);
+
+//reserved keyword cannot use
+let number = 10;
+
+//don't start from number
+//use related name
+
+let firstname = "Nisha";
+
+//camelcase
+let firstName = "Nisha";
+
+//primitives/value type
+//sring
+
+let firstName = "Nisha";
+let lastName = "pandey";
+
+//number
+
+let percentage = 26.78;
+
+//Boolean
+let idChecked = "false";
+
+//Undefine
+let number;
+//(Typeof number;)
+let number = undefine;
+
+//null
+let address = null;
+
+//object creating
+
+let person = {
+  firstName: "nisha",
+  lastName: "panday",
+  age: 20,
+};
+//change age
+person.age = 10;
+console.log(person.age);
+//or
+person["firstName"] = "samita";
+
+console.log("firstName");
