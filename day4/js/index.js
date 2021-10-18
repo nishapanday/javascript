beed42d60d17646ce34d2cc0a4761976e41c69b8
nisha.xdezo@gmail.com <nisha.xdezo@gmail.com>
2021-10-18 08:35:27 -0700

    loop

---
+++ day4/js/index.js
@@ -0,0 +1,55 @@
+// //if condition
+//  let x = 25;
+//  if (x > 10) console.log("x is greater than 10");
+
+// //if-else condition
+// let x = 5;
+// if (x > 10) {
+//   console.log("x is greater than 10");
+// } else {
+//   console.log("x is less than or equal 10");
+// }
+
+////2-3  if else condition
+// let a = 10,
+//   b = 20;
+// if (a > b) {
+//   console.log("a is greater than b");
+// } else if (a < b) {
+//   console.log("a is less than b");
+// } else {
+//   console.log("a is equal to b");
+// }
+
+////syntax
+// for (variable of iterable) {
+//     statement
+//   }
+//for loop
+// let array1 = ["a", "b", "c"];
+
+// for (let element of array1) {
+//   console.log(element);
+// }
+
+//for loop of string
+// let iterable = "boo";
+
+// for (let value of iterable) {
+//   console.log(value);
+// }
+//syntax
+// while (condition) statement;
+// // while loop
+// let n = 0;
+
+// while (n < 3) {
+//   n++;
+// }
+
+// console.log(n);
+
+// syntax do while loop
+// do
+//    statement
+// while (condition);
