fea80e7b983f3cdb22b457c27d4044f1b30768b5
nisha.xdezo@gmail.com <nisha.xdezo@gmail.com>
2021-10-18 07:37:30 -0700

    concatenate,another line,string

---
+++ day3/js/index.js
@@ -0,0 +1,26 @@
+// //string(primitives data type)
+// let x = 'Creating and "Selecting" a Database';
+
+// //
+// let value = 2;
+
+// let sentence = "Creating and" + value + "selecting a database";
+
+// //qu
+// const title = document.querySelector(".abc");
+
+// var value = 25;
+// let sentence = `Creating selected Database ${value}`;
+// title.innerHTML = sentence;
+
+//concatenate
+
+// const title = document.querySelector(".abc");
+
+// var value1 = 25;
+// var value2 = 2;
+// let sentence = `Creating selected Database ${value1 + value2}`;
+// title.innerHTML = sentence;
+
+//another line
+console.log("First Name: nisha" + "\n" + "Last Name: Panday");
